3ed2909f700550c49f26a8dd5be40978c2a42bde
時語 <a31602603@yahoo.com.hk>
2026-05-14 12:46:16 +0800

    chore: meleemode translate

---
+++ lang/zh_CN/Package/MeleeModePackage.lua
@@ -0,0 +1,12 @@
+return {
+	["MeleeMode"] = "鏖战模式",
+	["melee_peach"] = "鏖战",
+	[":melee_peach"] = "未转化的【桃】只能当做【杀】或【闪】使用或打出，不能用来回复体力。",
+	["melee_peach_filter"] = "鏖战",
+	[":melee_peach_filter"] = "锁定技，所有【桃】均被视为【杀/闪】，所有存活角色出杀次数+1。",
+	["#MeleeModeStart"] = "鏖战模式已启动，未转化的【桃】只能当做【杀】或【闪】使用或打出，不能用来回复体力。",
+	["#MeleeModeStartDoudizhu"] = "鏖战模式已启动，所有桃均被视为【杀/闪】，所有存活角色出杀次数+1",
+	["melee_slash_jink"] = "杀/闪",
+	[":melee_slash_jink"] = "基本牌<br /><b>卡牌效果</b>：此牌视为【杀】和【闪】的使用效果。",
+	["#MeleeModeStart"] = "鏖战模式已启动，【桃】视为【杀】或【闪】，每回合出【杀】次数+1",
+}
