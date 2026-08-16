02e0f6e02eefa3a32370bc507e7e0f8d16ff1c5b
時語 <a31602603@yahoo.com.hk>
2026-08-17 00:30:19 +0800

    refactor: extract Room player lifecycle management into PlayerLifecycleService
    - move player signup/add/reconnect, kill/revive/rest, changeHero, and mid-game summon/insert logic out of Room into PlayerLifecycleService with unit tests
    - introduce EventDispatcher abstraction implemented by Room to decouple lifecycle services from RoomThread triggers
    - wire service dependencies (roster, skill runtime, card movement, notifier) explicitly

---
+++ src/server/player-lifecycle-service.h
@@ -0,0 +1,73 @@
+#ifndef PLAYER_LIFECYCLE_SERVICE_H
+#define PLAYER_LIFECYCLE_SERVICE_H
+
+#include "structs.h"
+
+#include <QList>
+#include <QString>
+
+class CardMovementService;
+class ClientSocket;
+class EventDispatcher;
+class Room;
+class RoomNotifier;
+class RoomRoster;
+class SkillRuntimeCoordinator;
+
+class PlayerLifecycleService
+{
+public:
+    PlayerLifecycleService(Room &room, RoomRoster &roster,
+                           SkillRuntimeCoordinator &skillRuntime,
+                           CardMovementService &cardMovement,
+                           RoomNotifier &notifier,
+                           EventDispatcher &eventDispatcher);
+
+    ServerPlayer *addSocket(ClientSocket *socket);
+    ServerPlayer *addAIPlayer();
+    void signup(ServerPlayer *player, const QString &screenName,
+                const QString &avatar, bool isRobot);
+    void reconnect(ServerPlayer *player, ClientSocket *socket);
+    void marshal(ServerPlayer *player);
+
+    void killPlayer(ServerPlayer *victim, DamageStruct *reason, HpLostStruct *hpLost);
+    void revivePlayer(ServerPlayer *player, bool sendLog, bool throwMark, bool visibleOnly);
+    void restPlayer(ServerPlayer *player, const QString &reason, bool discardCards);
+    void directRestPlayer(ServerPlayer *player, const QString &reason, bool discardCards);
+    void unrestPlayer(ServerPlayer *player, bool restoreFullHp, bool restoreOriginalSkills);
+    bool isRest(ServerPlayer *player) const;
+    QList<ServerPlayer *> getRestPlayers() const;
+
+    void changeHero(ServerPlayer *player, const QString &newGeneral, bool fullState,
+                    bool invokeStart, bool isSecondaryHero, bool sendLog, int startHp);
+    void changePlayerGeneral(ServerPlayer *player, const QString &newGeneral);
+    void changePlayerGeneral2(ServerPlayer *player, const QString &newGeneral);
+
+    void requestSummonBetween(ServerPlayer *before, ServerPlayer *after,
+                              const QString &generalName);
+    bool hasPendingSummons() const;
+    void processPendingSummons();
+    ServerPlayer *insertPlayerMidGame(ServerPlayer *before, ServerPlayer *after,
+                                      const QString &generalName);
+
+private:
+    friend struct PlayerLifecycleServiceTestAccess;
+
+    struct SummonRequest
+    {
+        ServerPlayer *before;
+        ServerPlayer *after;
+        QString generalName;
+    };
+
+    Room &m_room;
+    RoomRoster &m_roster;
+    SkillRuntimeCoordinator &m_skillRuntime;
+    CardMovementService &m_cardMovement;
+    RoomNotifier &m_notifier;
+    EventDispatcher &m_eventDispatcher;
+    QList<SummonRequest> m_pendingSummons;
+    QList<ServerPlayer *> m_dynamicPlayers;
+};
+
+#endif
