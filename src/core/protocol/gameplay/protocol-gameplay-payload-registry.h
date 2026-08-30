5987105fede5f354169f112da59e3138bcc0e1a4
時語 <a31602603@yahoo.com.hk>
2026-08-30 12:53:01 +0800

    feat(protocol): add typed multiple-choice payload boundary

---
+++ src/core/protocol/gameplay/protocol-gameplay-payload-registry.h
@@ -0,0 +1,26 @@
+#ifndef PROTOCOL_GAMEPLAY_PAYLOAD_REGISTRY_H
+#define PROTOCOL_GAMEPLAY_PAYLOAD_REGISTRY_H
+
+#include "protocol/protocol-message.h"
+
+namespace QSanProtocol {
+
+class ProtocolGameplayPayloadRegistry
+{
+public:
+    static bool encodeForWire(ProtocolVersion activeVersion,
+                              const ProtocolMessage &logicalMessage,
+                              ProtocolMessage *wireMessage,
+                              QString *error = nullptr);
+    static bool decodeFromWire(ProtocolVersion activeVersion,
+                               const ProtocolMessage &wireMessage,
+                               ProtocolMessage *logicalMessage,
+                               QString *error = nullptr);
+
+    static bool isMigratedCommand(int command);
+    static int migratedCommandCount();
+};
+
+}
+
+#endif
