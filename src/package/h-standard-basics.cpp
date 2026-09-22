0c0cacc38970e9f3f12e228c69b74b781ec8696a
時語 <a31602603@yahoo.com.hk>
2026-09-22 14:28:15 +0800

    refactor(hegemony): Define basic deck with shared standard cards

---
+++ src/package/h-standard-basics.cpp
@@ -0,0 +1,100 @@
+/********************************************************************
+    Copyright (c) 2013-2014 - QSanguosha-Rara
+
+    This file is part of QSanguosha-Hegemony.
+
+    This game is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 3.0
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    See the LICENSE file for more details.
+
+    QSanguosha-Rara
+    *********************************************************************/
+
+#include "standard.h"
+#include "maneuvering.h"
+#include "h-standard-package.h"
+
+QList<Card *> HStandardCardPackage::basicCards() {
+    QList<Card *> cards;
+
+
+    cards
+        << new Slash(Card::Spade, 5)
+        << new Slash(Card::Spade, 7)
+        << new Slash(Card::Spade, 8)
+        << new Slash(Card::Spade, 8)
+        << new Slash(Card::Spade, 9)
+        << new Slash(Card::Spade, 10)
+        << new Slash(Card::Spade, 11)
+
+        << new Slash(Card::Club, 2)
+        << new Slash(Card::Club, 3)
+        << new Slash(Card::Club, 4)
+        << new Slash(Card::Club, 5)
+        << new Slash(Card::Club, 8)
+        << new Slash(Card::Club, 9)
+        << new Slash(Card::Club, 10)
+        << new Slash(Card::Club, 11)
+        << new Slash(Card::Club, 11)
+
+        << new Slash(Card::Heart, 10)
+        << new Slash(Card::Heart, 12)
+
+        << new Slash(Card::Diamond, 10)
+        << new Slash(Card::Diamond, 11)
+        << new Slash(Card::Diamond, 12)
+
+        << new FireSlash(Card::Heart, 4)
+
+        << new FireSlash(Card::Diamond, 4)
+        << new FireSlash(Card::Diamond, 5)
+
+        << new ThunderSlash(Card::Spade, 6)
+        << new ThunderSlash(Card::Spade, 7)
+
+        << new ThunderSlash(Card::Club, 6)
+        << new ThunderSlash(Card::Club, 7)
+        << new ThunderSlash(Card::Club, 8)
+
+        << new Jink(Card::Heart, 2)
+        << new Jink(Card::Heart, 11)
+        << new Jink(Card::Heart, 13)
+
+        << new Jink(Card::Diamond, 2)
+        << new Jink(Card::Diamond, 3)
+        << new Jink(Card::Diamond, 6)
+        << new Jink(Card::Diamond, 7)
+        << new Jink(Card::Diamond, 7)
+        << new Jink(Card::Diamond, 8)
+        << new Jink(Card::Diamond, 8)
+        << new Jink(Card::Diamond, 9)
+        << new Jink(Card::Diamond, 10)
+        << new Jink(Card::Diamond, 11)
+        << new Jink(Card::Diamond, 13)
+
+        << new Peach(Card::Heart, 4)
+        << new Peach(Card::Heart, 6)
+        << new Peach(Card::Heart, 7)
+        << new Peach(Card::Heart, 8)
+        << new Peach(Card::Heart, 9)
+        << new Peach(Card::Heart, 10)
+        << new Peach(Card::Heart, 12)
+
+        << new Peach(Card::Diamond, 2)
+
+        << new Analeptic(Card::Spade, 9)
+
+        << new Analeptic(Card::Club, 9)
+
+        << new Analeptic(Card::Diamond, 9);
+
+    return cards;
+}
